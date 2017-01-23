490bf43780b0836617163808856d2c26fbabb12d
kylemh <duskykmh@gmail.com>
2017-01-23 02:45:20 -0800

    Update gulpfile.js and requirements to Gulp4.0alpha

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var cleanCSS = require('gulp-clean-css');
-var rename = require("gulp-rename");
+var babel = require('gulp-babel')
 var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
-var bs = require('browser-sync').create();
+var rename = require('gulp-rename');
+var del = require('del');
+var bsync = require('browser-sync').create();
 
 // TODO: Implement unCSS, CSSO, and Critical
 
@@ -14,71 +16,66 @@ var bs = require('browser-sync').create();
 // $ gulp minify-images ::: optimizes various image files
 // $ gulp fonts ::: copies and moves fonts from src to public for deployment
 
+// Remove previous build
+function clean() {
+	return del(["dist"])
+}
 
-// Acts as quasi-main file - simply call $ gulp
-gulp.task('default', ['bsync', 'sass', ['minify-css', 'minify-js'], 'watch']);
-
-
-// Convert Sass/SCSS files to CSS
-gulp.task('sass', function(){
+// Convert Sass/SCSS files to CSS, minify them, and move them to dist
+function minify_css(){
 	return gulp.src("src/scss/*")
-	.pipe(sass()) // Converts Sass to CSS with gulp-sass
-	.pipe(gulp.dest("src/css"))
-	.pipe(bsync.reload({stream: true}));
-});
-
-
-// Compress .css files to min.css files and move minified files to distribution directory
-gulp.task('minify-css', function(){
-	return gulp.src("src/css")
+	.pipe(sass())
 	.pipe(cleanCSS())
-	.pip(rename({suffix: ".min"}))
-	.pipe(gulp.dest("dist/css")
-	.pipe(bsync.reload({stream: true}));
-});
-
+	.pipe(rename({basename: "main", suffix: ".min"}))
+	.pipe(gulp.dest("dist/css"))
+	.pipe(bsync.reload({stream: true}))
+};
 
 // Compress .js files to min.js files and move minified files to distribution directory
-gulp.task('minify-jss', function(){
+function minify_js(){
 	return gulp.src("src/js")
-	.pipe(concat("app.js"))
-	.pipe(gulp.dest("dist"))
+	.pipe(babeL())
 	.pipe(uglify())
-	.pipe(rename({suffix: ".min"}))
-	.pipe(gulp.dest("dist/js"));
-	.pipe(bsync.reload({stream: true}));
-});
-
+	.pipe(concat("main.min.js"))
+	.pipe(gulp.dest("dist/js"))
+	.pipe(bsync.reload({stream: true}))
+};
 
 // Optimize .png, .jpg, .jpeg, and .gif files
-gulp.task('minify-images', function(){
-	return gulp.src('public/*.+(png|jpg|jpeg|gif)')
+function minify_images(){
+	return gulp.src("public/*.+(png|jpg|jpeg|gif)")
 	.pipe(imagemin())
-	.pipe(gulp.dest('public/img'))
-});
-
+	.pipe(gulp.dest("public/img"))
+};
 
 // Sync browser with project
-gulp.task('bsync', function() {
-	bsync.init({
-		server: {
-			baseDir: "./"
-		},
-	})
-});
-
+function bsync() {
+	bsync.init({server: {baseDir: "./"},})
+};
 
 // Copy font files from source to public
-gulp.task('fonts', function() {
+function fonts() {
 	return gulp.src("src/fonts/*")
 	.pipe(gulp.dest("public/fonts"))
-})
-
+}
 
 // Gulp watcher for realtime changes
-gulp.task('watch', function() {
-    gulp.watch("src/scss/*", ['sass']);
-	gulp.watch("src/css/*", ['minify-css']);
-	gulp.watch("src/js/*", ['minify-js']);
-    gulp.watch("*.html").on('change', bsync.reload);
-});
+function watch() {
+    gulp.watch("src/scss/*", minify_css)
+	gulp.watch("src/js/*", minify_js)
+    gulp.watch("*.html").on('change', bsync.reload)
+};
+
+// Task declarations
+exports.clean = clean;
+exports.minify_css = minify_css;
+exports.minify_js = minify_js;
+exports.minify_images = minify_images;
+exports.bsync = bsync;
+exports.fonts = fonts;
+exports.watch = watch;
+
+var minify = gulp.parallel(minify_css, minify_js);
+var build = gulp.series(clean, bsync, minify, watch);
+gulp.task('build', build);
+gulp.task('default', build);
