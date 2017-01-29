bcb904a344312d42988b3b69934085efd275d4ef
kylemh <kylemh.email@gmail.com>
2017-01-29 06:54:43 -0800

    Complete svg.map and finish gulpfile.js
    Finishing gulpfile.js is easily understood, but after dusting and minimizing every SVG file, I discovered that there was a way to "stack them" in single file. Now, I can do cached references to SVG files so that my HTML file looks a little cleaner... tl;dr: no more ugly, inline SVGs.

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,18 @@ var sass = require('gulp-sass');
 var babel = require('gulp-babel');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
 var cleanCSS = require('gulp-clean-css');
 var del = require('del');
-var bsync = require('browser-sync');
+var bsync = require('browser-sync').create();
 
 var paths = {
 	styles: {
-		src: 'src/scss/*.scss',
-		dest: 'dist/css/'
+		src: './src/scss/**/*.{scss,sass}',
+		dest: './dist/css/'
 	},
 	scripts: {
-		src: 'src/js/**/*.js',
-		dest: 'dist/js/'
+		src: './src/js/**/*.js',
+		dest: './dist/js/'
 	}
 };
 
@@ -28,31 +27,39 @@ function clean() {
 function styles() {
 	return gulp.src(paths.styles.src)
 	.pipe(sass())
+	.pipe(concat('main.min.css'))
 	.pipe(cleanCSS())
-	.pipe(rename({basename: 'main',suffix: '.min'}))
-	.pipe(gulp.dest(paths.styles.dest));
+	.pipe(gulp.dest(paths.styles.dest))
+	.pipe(bsync.stream());
+	// .pipe(bsync.reload({stream:true}));
 }
 
 // Compress .js files to min.js files and move minified files to distribution directory
 function scripts() {
-  return gulp.src(['./src/js/jquery.js', paths.scripts.src], {sourcemaps: true})
+	return gulp.src(['./src/js/jquery.js', paths.scripts.src], {sourcemaps: true})
     .pipe(babel())
     .pipe(uglify())
     .pipe(concat('main.min.js'))
-    .pipe(gulp.dest(paths.scripts.dest));
+    .pipe(gulp.dest(paths.scripts.dest))
+    .pipe(bsync.stream());
+    // .pipe(bsync.reload({stream:true}));
 }
 
 // Sync browser with project
-gulp.task('server', function(done) {
-     bSync({server: {baseDir: ['./']}})
-     done();
-})
+// gulp.task('server', function() {
+//      bsync({server: {baseDir: ['./']}})
+//      done();
+// })
 
 // Gulp watcher for realtime changes
 function watch() {
+	bsync.init({
+		server: './'
+	})
+
 	gulp.watch(paths.scripts.src, scripts);
 	gulp.watch(paths.styles.src, styles);
-	gulp.watch('./*.html').on('changes', bsync.reload);
+	gulp.watch('*.html').on('change', bsync.reload);
 }
 
 // Generate tasks
@@ -61,7 +68,6 @@ exports.styles = styles;
 exports.scripts = scripts;
 exports.watch = watch;
 
-var build = gulp.series(clean, gulp.parallel(styles, scripts));
-
+var build = gulp.series(gulp.parallel(styles, scripts));
 gulp.task('build', build);
 gulp.task('default', gulp.series(clean, build, watch));
